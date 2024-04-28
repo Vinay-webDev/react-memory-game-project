10777a942d6faf2f70d204456588f643bacbbd17
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-29 01:48:12 +0530

    1. 💫magic-memory💫
    💫magic-memory💫 ===>>> 1. file tree, font, some basic css

---
+++ rct-memory-game/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="app" > 
+        <h1> 💫magic match💫 </h1>
+        <button> new game </button>
+      </div>
+    </>
+  )
+}
+
+export default App
